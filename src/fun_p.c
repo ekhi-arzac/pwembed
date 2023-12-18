134f2a851d8a4c0a716fc314303391faca7d3142
mfss0412 <mfss0412@estudianTEC.cr>
2023-12-18 11:59:00 +0100

    2da version paralelizaciob
    se ha paralelizado a mas profundidad el codigo

--- src/fun_p.c
+++ src/fun_p.c
@@ -6,21 +6,25 @@
 #include "defineg.h" // definiciones
 #include <float.h>
 #include <math.h>
-#include <omp.h>
 #include <stdlib.h>
+
+// double inline pow2(double n)
+// {
+//   return n * n;
+// }
 
 /*******************************************************************
  1 - Funcion para calcular la distancia euclidea entre dos vectores
  Entrada: 2 elementos con NDIM caracteristicas (por referencia)
  Salida:  distancia (double)
 ********************************************************************/
-double gendist(float *vec1, float *vec2) {
+double gendist(float *vec1, float *vec2)
+{
   double distancia = 0.0;
   int i;
-
-#pragma omp for private(i) reduction(+ : distancia)
-  for (i = 0; i < NDIM; i++) {
-    distancia += (vec2[i] - vec1[i]) * (vec2[i] - vec1[i]);
+  for (i = 0; i < NDIM; i++)
+  {
+    distancia += (vec2[i] - vec1[i])*(vec2[i] - vec1[i]);
   }
   return sqrt(distancia);
 }
@@ -33,20 +37,27 @@ ngrupos x NDIM, por referencia Salida:  popul grupo mas cercano a cada elemento,
 vector de tamanno MAXV, por ref.
 ************************************************************************************/
 void grupo_cercano(int nvec, float mvec[][NDIM], float cent[][NDIM],
-                   int *popul) {
+                   int *popul)
+{
   int i, j;
-  for (i = 0; i < nvec; i++) {
+  #pragma omp parallel for private(i, j)
+  for (i = 0; i < nvec; i++)
+  {
     double min_dist = DBL_MAX; // Infinito
     int min_index = -1;
-    for (j = 0; j < ngrupos; j++) {
+    for (j = 0; j < ngrupos; j++)
+    {
       double distancia = gendist(mvec[i], cent[j]);
-      if (distancia < min_dist) {
+      if (distancia < min_dist)
+      {
         min_dist = distancia;
         min_index = j;
       }
+      
     }
     popul[i] = min_index;
   }
+  
 }
 
 /***************************************************************************************
@@ -61,7 +72,8 @@ referencia Salida:  valor del CVI (double): calidad/bondad de la particion de
 clusters
 ****************************************************************************************/
 double silhouette_simple(float mvec[][NDIM], struct lista_grupos *listag,
-                         float cent[][NDIM], float a[]) {
+                         float cent[][NDIM], float a[])
+{
 
   int i, j, k;
   double distancia;
@@ -74,15 +86,22 @@ double silhouette_simple(float mvec[][NDIM], struct lista_grupos *listag,
   //   	si el numero de elementos del grupo es 0 o 1, densidad = 0
   // ...
   int nvecg;
-  for (i = 0; i < ngrupos; i++) {
+  #pragma omp parallel for private(i, j, k, distancia, nvecg) shared(a, mvec, listag)
+  for (i = 0; i < ngrupos; i++)
+  {
     nvecg = listag[i].nvecg;
 
-    if (nvecg <= 1) {
+    if (nvecg <= 1)
+    {
       a[i] = 0;
-    } else {
+    }
+    else
+    {
       distancia = 0;
-      for (j = 0; j < listag[i].nvecg; j++) {
-        for (k = j + 1; k < listag[i].nvecg; k++) {
+      for (j = 0; j < nvecg; j++)
+      {
+        for (k = j + 1; k < nvecg; k++)
+        {
           distancia +=
               gendist(mvec[listag[i].vecg[k]], mvec[listag[i].vecg[j]]);
         }
@@ -92,10 +111,12 @@ double silhouette_simple(float mvec[][NDIM], struct lista_grupos *listag,
   }
   // aproximar b[i] de cada cluster
   // ...
-
-  for (i = 0; i < ngrupos; i++) {
+  #pragma omp parallel for private(i, j, distancia) shared(b, cent)
+  for (i = 0; i < ngrupos; i++)
+  {
     distancia = 0;
-    for (j = 0; j < ngrupos; j++) {
+    for (j = 0; j < ngrupos; j++)
+    {
       if (i != j)
         distancia += gendist(cent[i], cent[j]);
     }
@@ -104,7 +125,9 @@ double silhouette_simple(float mvec[][NDIM], struct lista_grupos *listag,
   // calcular el ratio s[i] de cada cluster
   // ...
   double s = 0;
-  for (i = 0; i < ngrupos; i++) {
+  #pragma omp parallel for reduction(+ : s)
+  for (i = 0; i < ngrupos; i++)
+  {
     s += (b[i] - a[i]) / max(a[i], b[i]);
   }
   // promedio y devolver
@@ -113,14 +136,16 @@ double silhouette_simple(float mvec[][NDIM], struct lista_grupos *listag,
   return s;
 }
 
-void swap(float *arr, int i, int j) {
+void swap(float *arr, int i, int j)
+{
   float temp = arr[i];
   arr[i] = arr[j];
   arr[j] = temp;
 }
 
 // A function to implement bubble sort
-void bubbleSort(float arr[], int n) {
+void bubbleSort(float arr[], int n)
+{
   int i, j;
   for (i = 0; i < n - 1; i++)
     for (j = 0; j < n - i - 1; j++)
@@ -136,7 +161,8 @@ por ref. mcam     campos, una matriz de tamaño MAXV x NCAM, por referencia
 por ref.
 *****************************************************************************************/
 void analisis_campos(struct lista_grupos *listag, float mcam[][NCAM],
-                     struct analisis *info_cam) {
+                     struct analisis *info_cam)
+{
   // PARA COMPLETAR
   // Realizar el analisis de campos UNESCO en los grupos:
   //    mediana maxima y el grupo en el que se da este maximo (para cada campo)
@@ -144,33 +170,39 @@ void analisis_campos(struct lista_grupos *listag, float mcam[][NCAM],
 
   int i, j, k, grp_max, grp_min, dim;
   float mediana_max, mediana_min;
-
-  for (i = 0; i < NCAM; i++) {
+  #pragma omp parallel for private(i, j, k, grp_max, grp_min, dim, mediana_max, mediana_min)
+  for (i = 0; i < NCAM; i++)
+  {
 
     mediana_max = -1;
     mediana_min = FLT_MAX;
     grp_max = -1;
     grp_min = -1;
 
-    for (j = 0; j < ngrupos; j++) {
+    for (j = 0; j < ngrupos; j++)
+    {
 
       dim = listag[j].nvecg;
-      if (dim != 0) {
+      if (dim != 0)
+      {
         float campo_grp[dim];
 
-        for (k = 0; k < dim; k++) {
+        for (k = 0; k < dim; k++)
+        {
           campo_grp[k] = mcam[listag[j].vecg[k]][i];
         }
 
         bubbleSort(campo_grp, dim);
         float mediana = campo_grp[dim / 2];
 
-        if (mediana > mediana_max) {
+        if (mediana > mediana_max)
+        {
           grp_max = j;
           mediana_max = mediana;
         }
 
-        if (mediana < mediana_min) {
+        if (mediana < mediana_min)
+        {
           grp_min = j;
           mediana_min = mediana;
         }
@@ -186,12 +218,14 @@ void analisis_campos(struct lista_grupos *listag, float mcam[][NCAM],
 /*************************************
    OTRAS FUNCIONES DE LA APLICACION
 **************************************/
-void inicializar_centroides(float cent[][NDIM]) {
+void inicializar_centroides(float cent[][NDIM])
+{
   int i, j;
   float rand_val;
   srand(147);
   for (i = 0; i < ngrupos; i++)
-    for (j = 0; j < NDIM / 2; j++) {
+    for (j = 0; j < NDIM / 2; j++)
+    {
       rand_val = ((rand() % 10000) / 10000.0) * 2 - 1;
       cent[i][j] = rand_val;
       cent[i][j + (NDIM / 2)] = cent[i][j];
@@ -199,18 +233,21 @@ void inicializar_centroides(float cent[][NDIM]) {
 }
 
 int nuevos_centroides(float mvec[][NDIM], float cent[][NDIM], int popul[],
-                      int nvec) {
+                      int nvec)
+{
   int i, j, fin;
   double discent;
   double additions[ngrupos][NDIM + 1];
   float newcent[ngrupos][NDIM];
+
 
   for (i = 0; i < ngrupos; i++)
     for (j = 0; j < NDIM + 1; j++)
       additions[i][j] = 0.0;
 
   // acumular los valores de cada caracteristica; numero de elementos al final
-  for (i = 0; i < nvec; i++) {
+  for (i = 0; i < nvec; i++)
+  {
     for (j = 0; j < NDIM; j++)
       additions[popul[i]][j] += mvec[i][j];
     additions[popul[i]][NDIM]++;
@@ -219,15 +256,18 @@ int nuevos_centroides(float mvec[][NDIM], float cent[][NDIM], int popul[],
   // calcular los nuevos centroides y decidir si el proceso ha finalizado o no
   // (en funcion de DELTA)
   fin = 1;
-  for (i = 0; i < ngrupos; i++) {
-    if (additions[i][NDIM] > 0) { // ese grupo (cluster) no esta vacio
+  for (i = 0; i < ngrupos; i++)
+  {
+    if (additions[i][NDIM] > 0)
+    { // ese grupo (cluster) no esta vacio
       // media de cada caracteristica
       for (j = 0; j < NDIM; j++)
         newcent[i][j] = (float)(additions[i][j] / additions[i][NDIM]);
 
       // decidir si el proceso ha finalizado
       discent = gendist(&newcent[i][0], &cent[i][0]);
-      if (discent > DELTA1) {
+      if (discent > DELTA1)
+      {
         fin = 0; // en alguna centroide hay cambios; continuar
       }
 
